4e826b64481e289a8d1a72f1cff690bfeca05252
SimLif <517992857@qq.com>
2021-08-18 17:02:35 +0800

    初步完成空调和灯的遥控器

--- src/views/nurses/Patients.vue
+++ src/views/nurses/Patients.vue
@@ -1,10 +1,130 @@
 <template>
   <div class="dashboard-container ">
-
+    
+    <!-- 设备管理对话框 -->
     <el-dialog
       :visible.sync="deviceInfoVisible"
       width="60%"
       >
+      <!-- 嵌套在设备管理对话框中的阈值设置对话框 -->
+      <el-dialog
+        :visible.sync="deviceThresholdVisible"
+        width="40%"
+        append-to-body
+        :before-close="cancelThreshold"
+        >
+        <!-- <span>这是一段信息</span> -->
+
+        <div class="ui grid">
+          <div class="eight wide column">
+            <h1 class="ui header">阈值设定</h1>
+          </div>
+          <div class="eight wide column">
+            <div class="ui right aligned container">
+              <button @click="confirmThreshold" class="ui labeled icon primary button">
+                <i class="save outline icon"></i>
+                保存
+              </button>
+              <button @click="cancelThreshold" class="ui labeled icon button">
+                <i class="trash alternate outline icon"></i>
+                取消
+              </button>
+
+            </div>
+          </div>
+        </div>
+        <div class="ui divider"></div>
+        
+        <div class="ui grid">
+          <div class="eight wide column">
+            <h4 class="ui header">最小值</h4>
+          </div>
+          <div class="eight wide column">
+            <div class="ui right aligned container">
+              <span>单位：{{ tableData[0].unit }}</span>
+            </div>
+          </div>
+          <div class="sixteen wide column">
+            <div class="ui text segment" style="margin-top:-4%">
+              <el-slider v-model="tableData[0].min_value" :min="min_threshold_min" :max="min_threshold_max" show-input></el-slider>
+            </div>
+          </div>
+          <!-- <div class="ui divider"></div> -->
+          <div class="eight wide column">
+            <h4 class="ui header">最大值</h4>
+          </div>
+          <div class="eight wide column">
+            <div class="ui right aligned container">
+              <span>单位：{{ tableData[0].unit }}</span>
+            </div>
+          </div>
+          <div class="sixteen wide column">
+            <div class="ui text segment" style="margin-top:-4%">
+              <el-slider v-model="tableData[0].max_value" :min="max_threshold_min" :max="max_threshold_max" show-input></el-slider>
+            </div>
+          </div>
+          
+        </div>
+        
+      </el-dialog>
+
+      <!-- 嵌套在设备管理对话框中的空调遥控对话框 -->
+      <el-dialog
+        :visible.sync="controlAirConVisible"
+        width="30%"
+        append-to-body
+        >
+        <!-- <span>这是一段信息</span> -->
+        <div class="ui right aligned segment" style=" overflow:auto; background:#f9fafb;">
+          <div class="ui huge header" style="font-family: LCD;">26</div>
+          
+          <div class="ui grid">
+            <div class="ui eight wide column">
+              <div class="ui left aligned container">
+                制冷
+              </div>
+            </div>
+            <div class="ui eight wide column">
+              ℃
+            </div>
+          </div>
+        </div>
+
+        <div class="ui divider"></div>
+        <el-button class="fluid ui positive button" >模式</el-button>
+        <el-button class="fluid ui icon primary basic  button" style="margin-top:2%"><i class="chevron up icon"></i></el-button>
+        <el-button class="fluid ui icon negative basic button" style="margin-top:2%"><i class="chevron down icon"></i></el-button>
+        
+      </el-dialog>
+
+      <!-- 嵌套在设备管理对话框中的灯遥控对话框 -->
+      <el-dialog
+        :visible.sync="controlLightVisible"
+        width="30%"
+        append-to-body
+        >
+        <!-- <span>这是一段信息</span> -->
+
+        <div class="ui right aligned padded segment" style=" overflow:auto; background:#f9fafb;">
+          <div class="ui grid">
+            <div class="ui eight wide column">
+              <div class="ui left aligned container">
+                <div class="ui huge header" style="font-family: LCD;">{{tableData[2].data}}</div>
+              </div>
+            </div>
+            <div class="ui eight wide column">
+              <span style="font-size: 60%;">单位：{{tableData[2].unit}}</span>
+            </div>
+          </div>
+        </div>
+        <div class="ui divider"></div>
+
+            <el-slider v-model="tableData[2].data" :min="tableData[2].min_value" :max="tableData[2].max_value"></el-slider>
+          
+      </el-dialog>
+      
+      
+      <!-- 设备管理对话框的主体 -->
       <!-- <span>这是一段信息</span> -->
 
       <div class="ui grid">
@@ -62,7 +182,7 @@
           <el-table-column label="操作" align="center">
             <template slot-scope="scope">
               <button class="ui small positive basic button">开关</button>
-              <button @click="activeThreshold" class="ui small primary basic button">控制</button>
+              <button @click="judgmentType(scope.row.name)" class="ui small primary basic button">控制</button>
               <button class="ui small negative basic button">移除</button>
               <!-- <el-button
                 size="mini"
@@ -89,6 +209,9 @@
       :visible.sync="deviceAddVisible"
       width="60%"
       >
+
+      
+
       <!-- <span>这是一段信息</span> -->
 
       <div class="ui grid">
@@ -169,64 +292,9 @@
       
     </el-dialog>
 
-    <el-dialog
-      :visible.sync="deviceThresholdVisible"
-      width="40%"
-      >
-      <!-- <span>这是一段信息</span> -->
+    
 
-      <div class="ui grid">
-        <div class="eight wide column">
-          <h1 class="ui header">阈值设定</h1>
-        </div>
-        <div class="eight wide column">
-          <div class="ui right aligned container">
-            <button @click="confirmThreshold" class="ui labeled icon primary button">
-              <i class="save outline icon"></i>
-              保存
-            </button>
-            <button @click="cancelThreshold" class="ui labeled icon button">
-              <i class="trash alternate outline icon"></i>
-              取消
-            </button>
-
-          </div>
-        </div>
-      </div>
-      <div class="ui divider"></div>
-      
-      <div class="ui grid">
-        <div class="eight wide column">
-          <h4 class="ui header">最小值</h4>
-        </div>
-        <div class="eight wide column">
-          <div class="ui right aligned container">
-            <span>单位：{{ tableData[0].unit }}</span>
-          </div>
-        </div>
-        <div class="sixteen wide column">
-          <div class="ui text segment" style="margin-top:-4%">
-            <el-slider v-model="tableData[0].min_value" :min="min_threshold_min" :max="min_threshold_max" show-input></el-slider>
-          </div>
-        </div>
-        <!-- <div class="ui divider"></div> -->
-        <div class="eight wide column">
-          <h4 class="ui header">最大值</h4>
-        </div>
-        <div class="eight wide column">
-          <div class="ui right aligned container">
-            <span>单位：{{ tableData[0].unit }}</span>
-          </div>
-        </div>
-        <div class="sixteen wide column">
-          <div class="ui text segment" style="margin-top:-4%">
-            <el-slider v-model="tableData[0].max_value" :min="max_threshold_min" :max="max_threshold_max" show-input></el-slider>
-          </div>
-        </div>
-        
-      </div>
-      
-    </el-dialog>
+    
 
     <div class="ui container">
         <div class = "ui four stackable cards">
@@ -297,6 +365,8 @@ export default {
           deviceInfoVisible : false,
           deviceAddVisible : false,
           deviceThresholdVisible : false,
+          controlAirConVisible : false,
+          controlLightVisible : false,
           tableData : [{
             id: 1,            
             name : "体温计",
@@ -305,6 +375,25 @@ export default {
             max_value : 123,
             min_value : 1,
             unit : "摄氏度",
+          },
+          {
+            id: 1,            
+            name : "空调",
+            type : 1,
+            on : false,
+            max_value : 123,
+            min_value : 1,
+            unit : "摄氏度",
+          },
+          {
+            id: 1,            
+            name : "灯",
+            type : 1,
+            on : false,
+            max_value : 123,
+            min_value : 1,
+            unit : "流明度",
+            data : 5,
           }],
           input : "",
           min_threshold_min:0,
@@ -328,9 +417,9 @@ export default {
       this.deviceInfoVisible = !this.deviceInfoVisible
       this.deviceAddVisible = !this.deviceAddVisible
     },
-    activeThreshold(e) {
-      this.deviceInfoVisible = !this.deviceInfoVisible
+    activeThreshold() {
       this.deviceThresholdVisible = !this.deviceThresholdVisible
+      // this.deviceInfoVisible = !this.deviceInfoVisible   
       this.max_value_backup = this.tableData[0].max_value
       this.min_value_backup = this.tableData[0].min_value
       this.min_threshold_min = this.tableData[0].min_value - 10
@@ -338,23 +427,35 @@ export default {
       this.max_threshold_min = this.tableData[0].max_value - 20
       this.max_threshold_max = this.tableData[0].max_value + 10
     },
-    cancelThreshold(e) {
-      this.deviceInfoVisible = !this.deviceInfoVisible
+    cancelThreshold() {
+      // this.deviceInfoVisible = !this.deviceInfoVisible
       this.deviceThresholdVisible = !this.deviceThresholdVisible
       this.tableData[0].max_value = this.max_value_backup  
       this.tableData[0].min_value = this.min_value_backup 
     },
-    confirmThreshold(e) {
-      this.deviceInfoVisible = !this.deviceInfoVisible
+    confirmThreshold() {
+      // this.deviceInfoVisible = !this.deviceInfoVisible
       this.deviceThresholdVisible = !this.deviceThresholdVisible
     },
-    handleClose(done) {
-        this.$confirm('确认关闭？')
-          .then(_ => {
-            done();
-          })
-          .catch(_ => {});
+    reverseLight() {
+      // this.deviceInfoVisible = !this.deviceInfoVisible
+      this.controlLightVisible = !this.controlLightVisible
+    },
+    reverseAirCon() {
+      // this.deviceInfoVisible = !this.deviceInfoVisible
+      this.controlAirConVisible = !this.controlAirConVisible
+    },
+    judgmentType(deviceName) {
+      if (deviceName == '空调') {
+        this.reverseAirCon()
       }
+      else if (deviceName == '灯') {
+        this.reverseLight()
+      }
+      else {
+        this.activeThreshold()
+      }
+    }
   }
 }
 </script>
